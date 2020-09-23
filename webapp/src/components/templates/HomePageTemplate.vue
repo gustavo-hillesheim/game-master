aa8b9cec5da9214f25a7c1c1f39c7a2b48dd0462
gustavo-hillesheim <guhillesheim@gmail.com>
2020-09-22 21:26:31 -0300

    Adicionado conteúdo à página inicial

---
+++ webapp/src/components/templates/HomePageTemplate.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page-content">
+    <div class="title-container">
+      <slot name="title"></slot>
+    </div>
+    <div class="content-container">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class HomePageTemplate extends Vue {}
+</script>
+<style scoped>
+.page-content {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.title-container {
+  margin: 16px 0;
+}
+.content-container {
+  margin: 32px auto;
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- webapp/src/pages/HomePage.vue
+++ webapp/src/pages/HomePage.vue
@@ -1,10 +1,26 @@
 <template>
-  <span>Home Page works!</span>
+  <HomePageTemplate>
+    <template #title>
+      <PageTitle>Game Master</PageTitle>
+    </template>
+    <template #content>
+      <pre>
+        Bem vindo ao Game Master!
+
+        Sou uma plataforma para resolução de jogos de tabuleiro,
+        você só precisa selecionar um jogo no menu ao lado, 
+        informe a entrada e me deixe resolvê-lo para você!
+      </pre>
+    </template>
+  </HomePageTemplate>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
-@Component
+import HomePageTemplate from "../components/templates/HomePageTemplate.vue";
+import PageTitle from "../components/atoms/PageTitle.vue";
+
+@Component({ components: { PageTitle, HomePageTemplate } })
 export default class HomePage extends Vue {}
 </script>
